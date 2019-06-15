1212899b63e35388918a69e5b6948e74d52d0d44
Zach Weisser <zweisser@gmail.com>
2019-06-15 12:49:54 -0400

    fix: fix improper assignment of package props

--- src/readfile.js
+++ src/readfile.js
@@ -1,26 +1,33 @@
 'use strict'
 const fs = require('fs-extra')
-const log = require('debug')('readQuery')
+const log = require('debug')('readfile')
 
 const Package = {
     error: Object,
     _error: Boolean,
     data: Object,
     datatype: String,
+    filepath: String,
 }
 
 async function readfile(filepath) {
     let pkg = { ...Package }
-    try {
-        log(filepath)
-
-        let filecontents = await fs.readFile(filepath, 'utf8')
-        pkg.data = filecontents
-        pkg.data = 'file:utf8'
-    } catch (err) {
-        log.extend('error')(err)
-        pkg.error = new Error(err)
+    if (!filepath) {
+        pkg.error = new Error(`No filename provided. (filepath: "${filepath}"`)
         pkg._error = true
+    } else {
+        try {
+            pkg.filepath = filepath
+            log.extend('filepath')(filepath)
+
+            let filecontents = await fs.readFile(filepath, 'utf8')
+            pkg.data = filecontents
+            pkg.datatype = 'file:utf8'
+        } catch (err) {
+            log.extend('error')(err)
+            pkg.error = new Error(err)
+            pkg._error = true
+        }
     }
     return pkg
 }
